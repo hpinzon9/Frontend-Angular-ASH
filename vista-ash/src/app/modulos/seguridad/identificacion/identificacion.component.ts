eaa0dc46afa94e6ada40f5f1f9b5f2618e5abe38
hpinzon9 <hpinzon9@gmail.com>
2022-11-08 12:12:14 -0500

    avance con frontend

--- vista-ash/src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ vista-ash/src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+import * as cryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-identificacion',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificacionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private servicioSeguridad: SeguridadService) { }
+
+  fgValidador: FormGroup = this.fb.group({
+    'usuario': ['', [Validators.required,Validators.email]],
+    'clave': ['', [Validators.required]]
+  });
 
   ngOnInit(): void {
+    // opcion logica de negocio
+    //setInterval(() => {
+    //  this.fgValidador.controls["usuario"].setValue(Math.random() * 1000)
+    //},2000)
+  }
+
+  IdentificarUsuario(){
+    let usuario = this.fgValidador.controls["usuario"].value;
+    let clave = this.fgValidador.controls["clave"].value;
+    let claveCifrada = CryptoJS.MD5(clave).toString();
+    this.servicioSeguridad.Identificar(usuario, claveCifrada).subscribe((datos: any) =>{
+      //ok
+      alert("Datos Correctos")
+    }, (error: any) => {
+      //error
+      alert("Datos Invalidos")
+    })
   }
 
 }
