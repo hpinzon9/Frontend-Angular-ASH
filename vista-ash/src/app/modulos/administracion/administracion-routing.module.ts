c6f9255e1fafe7a50e3da0d7ea4a531d0955c9fb
Mazx1 <111648060+Mazx1@users.noreply.github.com>
2022-11-10 16:08:42 -0500

    subiendo

--- vista-ash/src/app/modulos/administracion/administracion-routing.module.ts
+++ vista-ash/src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,16 +1,19 @@
 import { createComponent, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValidadorSesionGuard } from 'src/app/guardianes/validador-sesion.guard';
 import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
 import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
 
 const routes: Routes = [
   {
     path: 'crear-persona',
-    component: CrearPersonaComponent
+    component: CrearPersonaComponent,
+    canActivate: [ValidadorSesionGuard]
   },
   {
     path: 'editar-persona',
-    component: EditarPersonaComponent
+    component: EditarPersonaComponent,
+    canActivate: [ValidadorSesionGuard]
   }
 ];
 
