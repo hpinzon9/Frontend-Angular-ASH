958e3394ad1ad2290b224d293b6ef1a385bd0ac6
hpinzon9 <hpinzon9@gmail.com>
2022-11-15 20:54:19 -0500

    ajustes 15 nov 8-54 pm

--- vista-ash/src/app/modulos/administracion/administracion.module.ts
+++ vista-ash/src/app/modulos/administracion/administracion.module.ts
@@ -15,6 +15,10 @@ import { BuscarEmpresaComponent } from './empresa/buscar-empresa/buscar-empresa.
 import { EditarEmpresaComponent } from './empresa/editar-empresa/editar-empresa.component';
 import { EliminarEmpresaComponent } from './empresa/eliminar-empresa/eliminar-empresa.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CrearDirectivoComponent } from './directivos/crear-directivo/crear-directivo.component';
+import { BuscarDirectivoComponent } from './directivos/buscar-directivo/buscar-directivo.component';
+import { EditarDirectivoComponent } from './directivos/editar-directivo/editar-directivo.component';
+import { EliminarDirectivoComponent } from './directivos/eliminar-directivo/eliminar-directivo.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +33,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CrearEmpresaComponent,
     BuscarEmpresaComponent,
     EditarEmpresaComponent,
-    EliminarEmpresaComponent
+    EliminarEmpresaComponent,
+    CrearDirectivoComponent,
+    BuscarDirectivoComponent,
+    EditarDirectivoComponent,
+    EliminarDirectivoComponent
   ],
   imports: [
     CommonModule,
